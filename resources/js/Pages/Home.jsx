d500ae43b0b1f0a7a4ae8d64276ba78f0665b96c
faridanaksoleh <itspuyd@gmail.com>
2025-10-26 16:10:22 +0700

    feat: implement post creation functionality

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -7,16 +7,39 @@ export default function Home({ posts }) {
             <h1 className="title">Hello</h1>
 
             <div>
-                {posts.map((post) => (
+                {posts.data.map((post) => (
                     <div key={post.id} className="p-4 border-b">
                         <div className="text-sm text-slate-600">
                             <span>Posted on: </span>
-                            <span>{new Date(post.created_at).toLocaleTimeString()}</span>
+                            <span>
+                                {new Date(post.created_at).toLocaleTimeString()}
+                            </span>
                         </div>
                         <p className="font-medium">{post.body}</p>
                     </div>
                 ))}
             </div>
+
+            <div className="py-12 px-4">
+                {posts.links.map((link) => (
+                    link.url ? (
+                    <Link
+                        key={link.label}
+                        href={link.url || "#"}
+                        dangerouslySetInnerHTML={{ __html: link.label }}
+                        className={`py-2 px-4 ${link.active ? "text-blue-500 font-bold" : ""}`}
+                    />
+                ) : (    
+                    <span
+                        key={link.label}
+                        dangerouslySetInnerHTML={{ __html: link.label }}
+                        className="py-2 px-4 text-slate-300"
+                    >
+                    </span>
+                    )
+                ))}
+            </div>
         </>
     );
 }
+
